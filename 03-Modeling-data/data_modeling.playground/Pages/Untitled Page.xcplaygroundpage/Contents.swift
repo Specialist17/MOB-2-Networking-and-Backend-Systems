c8c2a64dd40019bcd9abc8695ed9a2930471a804
Specialist17 <arocho.fernando.13@gmail.com>
2017-09-18 22:15:26 -0700

    added more values to the struct (host name and isSuperHost)

--- 03-Modeling-data/data_modeling.playground/Pages/Untitled Page.xcplaygroundpage/Contents.swift
+++ 03-Modeling-data/data_modeling.playground/Pages/Untitled Page.xcplaygroundpage/Contents.swift
@@ -7,44 +7,73 @@ struct Listing {
     var name: String!
     var bedrooms: Int!
     var listings = [Listing]()
+    var hostFirstName: String!
+    var isSuperHost: Bool!
     
-    init(name: String, bedrooms: Int) {
+    init(name: String, bedrooms: Int, hostFirstName: String, isSuperHost: Bool) {
         self.name = name
         self.bedrooms = bedrooms
+        self.hostFirstName = hostFirstName
+        self.isSuperHost = isSuperHost
     }
 }
 
 extension Listing: Decodable {
     
+    // Main container - "search_results: []"
     enum SearchResultKeys: String, CodingKey {
         case search_results
         
+        // Second container - { listing: listing_properties,
+        //                      pricing_quote: values}
         enum ResultKeys: String, CodingKey {
             case listing
             
+            // Inside the listing container - "listing" : values
             enum ListingKeys: String, CodingKey {
                 case name
                 case bedrooms
+                case host = "primary_host"
+                
+                enum HostKeys: String, CodingKey {
+                    case hostFirstName = "first_name"
+                    case isSuperhost = "is_superhost"
+                }
             }
         }
     }
     
     init(from decoder: Decoder) throws {
         
+        // Go into main container
         var searchContainer = try decoder.container(keyedBy: SearchResultKeys.self)
        
+        // Get the array of unkeyed listings container
         var listingsContainer = try searchContainer.nestedUnkeyedContainer(forKey: .search_results)
         print(listingsContainer.count)
     
+        // loop through every listing element
         while !listingsContainer.isAtEnd {
+            
+            // access the current result container - {listing: value, pricing_quote: value}
             let resultContainer = try listingsContainer.nestedContainer(keyedBy: SearchResultKeys.ResultKeys.self)
             
+            // access the current listing container - "listing": values
             let container = try resultContainer.nestedContainer(keyedBy: SearchResultKeys.ResultKeys.ListingKeys.self, forKey: .listing)
 
+            // get specified values for the listing
             let name = try container.decode(String.self, forKey: .name)
             let bedrooms = try container.decode(Int.self, forKey: .bedrooms)
             
-            let listing = Listing(name: name, bedrooms: bedrooms)
+            let hostContainer = try container.nestedContainer(keyedBy: SearchResultKeys.ResultKeys.ListingKeys.HostKeys.self, forKey: .host)
+            
+            let hostName = try hostContainer.decode(String.self, forKey: .hostFirstName)
+            let isSuperHost = try hostContainer.decode(Bool.self, forKey: .isSuperhost)
+            
+            // initialize a listing object
+            let listing = Listing(name: name, bedrooms: bedrooms, hostFirstName: hostName, isSuperHost: isSuperHost)
+            
+            // add the listing object to the structs listings arrays
             self.listings.append(listing)
         }
     }
@@ -89,6 +118,8 @@ networking.getAnime(id: "1") { (res) in
     for listing in res.listings {
         print(listing.name)
         print(listing.bedrooms)
+        print(listing.hostFirstName)
+        print(listing.isSuperHost)
     }
 }
 
